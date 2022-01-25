85be2aec3226cba7862e709a9cfa941f7caf3126
KenleyL <greglenehan@hotmail.com>
2022-01-25 12:06:36 +0000

    "Third Commit"

--- Project.py
+++ Project.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import re
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 #importing the 4 csv files
 sales = pd.read_csv("sales.csv")
@@ -14,6 +17,12 @@ print(sales.head())
 print(customers.head())
 print(orders.head())
 print(products.head())
+
+#to know the names of the columns
+print(sales.columns)
+print(customers.columns)
+print(orders.columns)
+print(products.columns)
 
 #merging the sales, customers & Orders dataframes
 cust_orders = pd.merge(left=customers, right=orders,
@@ -59,6 +68,25 @@ sns.barplot(
 
 plt.show()
 
+#dropping duplicates. No duplicates in data
+drop_duplicates = cop_data.drop_duplicates()
+print(cop_data.shape,drop_duplicates.shape)
+
+mach_learn = cop_data[["sales", "product_ID"]]
+print(mach_learn)
+
+X = cop_data[["sales", "order_id"]]
+y = cop_data["product_ID"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+print(len(X_train))
+
+clf = LinearRegression()
+print(clf.fit(X_train, y_train))
+
+print(clf.predict(X_test))
+
+print(clf.score(X_test, y_test))
 
 
 
